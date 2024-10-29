fed1b7e5561bc87510c15b2cd850bbfbcae8631e
Piyush <piyushmalik1699@gmail.com>
2024-10-29 17:48:19 +0530

    test- change

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
-    
+
+a= 5
+b= 10
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
